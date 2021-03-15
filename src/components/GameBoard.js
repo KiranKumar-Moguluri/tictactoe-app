3fbee5dcd72aea53d0838fa11b0d49df0d267f5c
Stack_Pointer <kiranchamp.6457@gmail.com>
2021-03-15 20:18:01 +0530

    stuck in drawconditions

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -5,7 +5,7 @@ const GameBoard = () => {
 
     const [currentplayer, setcurrentplayer]=useState('X');
 
-    const[moves, setmoves]=useState(0);
+    const[moves, setmoves]= useState(0);
 
     const emptyGame=[{value:null}, {value:null}, {value:null}, {value:null}, {value:null}, {value:null}, {value:null}, {value:null}, {value:null}];
     const [gameState, setgameState]=useState(emptyGame);
@@ -20,14 +20,22 @@ const GameBoard = () => {
         let nextplayer= currentplayer==="X"? "O":"X";
         setcurrentplayer(nextplayer);
             let result = winorDraw();
-            if(result!=false)
+            
+           
+            if(result!=false && result!=null)
             {
                 alert(`Player ${result} wins !`);
+                setgameState(emptyGame)
+                setmoves(0);
+            }else{
+                let move_count= moves+1;
+                setmoves(move_count);
             }
-            let move_count=moves+1;
-            setmoves(move_count);
-            if(move_count===9){
+            
+            if(moves===9){
                 alert('Drawn!');
+                setgameState(emptyGame)
+                setmoves(0);
             }
         
         }
@@ -40,12 +48,33 @@ const GameBoard = () => {
         if(gameState[0].value===gameState[1].value && gameState[0].value===gameState[2].value){
             win=gameState[0].value;
         }
-       else if(gameState[0].value===gameState[4].value && gameState[4].value===gameState[8].value){
+        else if(gameState[0].value===gameState[4].value && gameState[4].value===gameState[8].value){
             win=gameState[0].value;
         }
-        else if(gameState[0].value===gameState[1].value && gameState[0].value===gameState[2].value){
+        else if(gameState[0].value===gameState[3].value && gameState[0].value===gameState[6].value){
             win=gameState[0].value;
         }
+        else if(gameState[1].value===gameState[4].value && gameState[4].value===gameState[7].value){
+            win=gameState[1].value;
+        }
+        else if(gameState[2].value===gameState[5].value && gameState[2].value===gameState[8].value){
+            win=gameState[2].value;
+        }
+        else if(gameState[2].value===gameState[4].value && gameState[4].value===gameState[6].value){
+            win=gameState[2].value;
+        }
+        else if(gameState[3].value===gameState[4].value && gameState[3].value===gameState[5].value){
+            win=gameState[3].value;
+        }
+        else if(gameState[6].value===gameState[7].value && gameState[6].value===gameState[8].value){
+            win=gameState[6].value;
+        }
+        else{
+                 alert('Drawn!');
+               
+        }
+        
+        
 
         //console.log(win);
 
@@ -56,7 +85,7 @@ const GameBoard = () => {
         <>
             <div className="col-md-12 col-12 text-center">
                 <h3>Current Players :{currentplayer}</h3>
-                <button onClick={e => setgameState(emptyGame)}>Restart</button>
+                <button onClick={e => {setgameState(emptyGame); setmoves(0)}}>Restart</button>
             </div>
 
             <div className="bg-white col-md-6 offset-md-3 GameBoard shadow-sm row p-4">
